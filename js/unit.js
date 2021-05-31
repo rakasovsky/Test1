71231792e1b3299bbdd233d7e7f61afec284d6f7
R <rakasovsky1@gmail.com>
2021-05-31 14:37:14 +0300

    js

--- js/unit.js
+++ js/unit.js
@@ -2,7 +2,7 @@ const tl = gsap.timeline();
 
 // tl.from(".hero", {duration: 2,   x: 1500});
 tl.from(".hero_icon",{duration:2, opacity:0, ease: "elastic.out", y:50, stagger: 0.55});
-gsap.from(".description-title", {duration: 2,  x: -1500, stagger: 0.55});
+// gsap.from(".description-title", {duration: 2,  x: -1500, stagger: 0.55});
 
 
 // О проекте
@@ -31,7 +31,8 @@ catBlck.from(".yellow_stripe",{duration:2, opacity:0, x:-200, stagger:0.5});
 const variants = gsap.timeline({
     scrollTrigger: {
         trigger: '.variants_wrapper',
-        start: "top bottom"
+        start: "center bottom",
+        toggleActions: "restart"
     }
 }
 )
@@ -45,12 +46,13 @@ variants.from(".vrsg2",{duration:1, opacity:0, x:-200})
 const style = gsap.timeline({
     scrollTrigger: {
         trigger: '.style_center',
-        start: "top bottom"
+        start: "top bottom",
+        toggleActions: "restart"
     }
 }
 )
 style.from(".rec1g",{duration:1, opacity:0, x:-800})
-style.from(".lineg",{duration:1, opacity:0, x:-500, stagger:0.8},"-=0.6")
+style.from(".lineg",{duration:1, opacity:0, x:-500, stagger:0.2},"-=0.2")
 style.from(".pre1g",{duration:1, opacity:0, x:-500})
 style.from(".circleg",{duration:1, opacity:0, x:-500})
 
@@ -58,7 +60,8 @@ style.from(".circleg",{duration:1, opacity:0, x:-500})
 const whywe = gsap.timeline({
     scrollTrigger: {
         trigger: '.whywe_center',
-        start: "top bottom"
+        start: "center bottom",
+        toggleActions: "restart"
     }
 }
 )
@@ -72,21 +75,23 @@ whywe.from(".rec2g",{duration:2, opacity:0, x:500},"-=1.3")
 const pos = gsap.timeline({
     scrollTrigger: {
         trigger: '.posability_center',
-        start: "top bottom"
+        start: "top bottom",
+        toggleActions: "restart"
     }
 }
 )
-const reversedChars = 
-pos.from(".pos1g",{duration:2, opacity:0, x:-500})
-pos.from(".pre3g",{duration:2, opacity:0, x:-500})
-pos.from(".rec3g",{duration:2, opacity:0, x:500})
-pos.from(".icons2",{duration:1, opacity:0, x:200, stagger:{each: -0.4}})
+
+pos.from(".pos1g",{duration:1, opacity:0, x:-500})
+pos.from(".pre3g",{duration:1, opacity:0, x:-500})
+pos.from(".rec3g",{duration:1, opacity:0, x:500})
+pos.from(".icons2",{duration:1, opacity:0, x:500, stagger:{each: -0.2}})
 
 // Экосистема
 const eco = gsap.timeline({
     scrollTrigger: {
         trigger: '.eco_center',
-        start: "top bottom"
+        start: "top bottom",
+        toggleActions: "restart"
     }
 }
 )
@@ -99,14 +104,15 @@ eco.from(".ecog",{duration:2, opacity:0, x:-500})
 const off = gsap.timeline({
     scrollTrigger: {
         trigger: '.center',
-        start: "top bottom"
+        start: "top bottom",
+        toggleActions: "restart"
     }
 }
 )
 
-off.from(".lampg",{duration:2, opacity:0, y:-500})
-off.from(".lightg",{duration:2, opacity:0, ease: "yoyo", repeat:1})
-off.from(".pre5g",{duration:2, opacity:0, x:-500})
+off.from(".lampg",{duration:1, opacity:0, y:-200})
+off.from(".lightg",{duration:1, opacity:0, ease: "yoyo", repeat:1})
+off.from(".pre5g",{duration:1, opacity:0, x:-500})
 off.from(".officeg",{duration:2, opacity:0, x:500},"-=1")
 off.from(".chair2g",{duration:2, opacity:0, x:500},"-=2")
 
@@ -115,10 +121,11 @@ off.from(".chair2g",{duration:2, opacity:0, x:500},"-=2")
 const we = gsap.timeline({
     scrollTrigger: {
         trigger: '.center',
-        start: "top bottom"
+        start: "center bottom",
+        toggleActions: "restart"
     }
 }
 )
-we.from(".we_rect",{duration:2, opacity:0, x:-500})
-we.from(".we_cush",{duration:2, opacity:0, x:-500},"-=2")
+we.from(".we_rect",{duration:2, opacity:0, x:-500},"-=1")
+we.from(".we_cush",{duration:1, opacity:1, x:-500},"-=2")
 we.from(".pre6g",{duration:2, opacity:0, x:500})
